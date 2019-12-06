090433c31c699ccd6f68dbb99c7e8669bd339f95
Natithan <nathan.5005@gmail.com>
2019-12-06 18:23:02 +0100

    Added albert embedder & some training lines

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals, print_function
 
 import os
 from pathlib import Path
+from typing import Any, T_co
 
+from allennlp.data.iterators import BucketIterator
+from allennlp.training import Trainer
 from spacy.lang.en import English  # updated
 import matplotlib
 import matplotlib.pyplot as plt
@@ -14,6 +17,8 @@ from allennlp.data.token_indexers import SingleIdTokenIndexer
 matplotlib.use('TkAgg')
 import torch
 import torch.nn as nn
+import torch.optim as optim
+
 import nltk
 from torch.nn.modules.activation import MultiheadAttention
 from absl import app
@@ -22,12 +27,17 @@ from absl import flags
 # %% FLAGS
 FLAGS = flags.FLAGS
 flags.DEFINE_integer("d_batch", 3, "Batch size")
-flags.DEFINE_integer("d_emb", 16, "Embedding size")
+flags.DEFINE_integer("d_emb", 12, "Size of token encodings before contextualization")
+flags.DEFINE_integer("d_hidden", 72, "Size of token encodings in hidden layers (contextualized)")
 flags.DEFINE_integer("nb_heads", 8, "Number of attention heads")
 flags.DEFINE_integer("target_length", 20, "Number of tokens in target sequence")
 flags.DEFINE_integer("source_length", 20, "Number of tokens in source sequence")
 flags.DEFINE_integer("max_seq_length", 20, "Maximum number of words to consider per batch")
 flags.DEFINE_string("data_folder", "./data/Gutenberg", "Folder with train, val and test subfolders containing data")
+
+# Trainer flags
+flags.DEFINE_integer("patience", 10, "Number of epochs the validation metric can worsen before stopping training.")
+flags.DEFINE_integer("num_epochs", 1000, "Numnber of epochs to train for.")
 
 flags.DEFINE_bool("mini", True, "Whether to work with mini data/models for debugging purposes")
 
@@ -112,13 +122,53 @@ class MultiHeadAttention(nn.Module):
         return att_output
 
 
+class AlbertEmbedder(nn.Module):
+    """
+    Factorized embedder, as proposed in the ALBERT paper: http://arxiv.org/abs/1909.11942
+    """
+    def __init__(self,vocab):
+        super().__init__()
+        self.index_to_embedding = nn.Linear(vocab.get_vocab_size('tokens'), FLAGS.d_emb)
+        self.embedding_to_hidden = nn.Linear(FLAGS.d_emb, FLAGS.d_hidden)
+    def forward(self, input):
+        return self.embedding_to_hidden(self.index_to_embedding(input))
+
+
+
+class MyModel(nn.Module):
+    def __init__(self,vocab):
+        super().__init__()
+        self.embedder = AlbertEmbedder(vocab)
+
+    def forward(self, *input: Any, **kwargs: Any) -> T_co:
+
+
+
 def main(_):
     reader = GutenbergReader()
     train_dataset = reader.read(os.path.join(FLAGS.data_folder,'train'))
     test_dataset = reader.read(os.path.join(FLAGS.data_folder,'test'))
     val_dataset = reader.read(os.path.join(FLAGS.data_folder,'val'))
-    vocab = Vocabulary.from_instances(train_dataset+val_dataset)
+    vocab = Vocabulary.from_instances(train_dataset + val_dataset)
 
+    model = MyModel(vocab)
+    cuda_device = 0
+    model = model.cuda(cuda_device)
+
+    optimizer = optim.Adam()
+
+    iterator = BucketIterator(batch_size=FLAGS.d_batch, sorting_keys=[("sentence", "num_tokens")])
+    iterator.index_with(vocab)
+
+    trainer = Trainer(model=model,
+                      optimizer=optimizer,
+                      iterator=iterator,
+                      train_dataset=train_dataset,
+                      validation_dataset=val_dataset,
+                      patience=FLAGS.patience,
+                      num_epochs=FLAGS.num_epochs,
+                      cuda_device=cuda_device)
+    trainer.train()
     input = torch.rand(FLAGS.d_batch, FLAGS.source_length, FLAGS.d_emb)
     output = AttentionLayer()(input)
     # loss =
